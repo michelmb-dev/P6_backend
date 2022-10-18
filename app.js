4d46d86e153534860d9459b2134a50d7c135e3aa
Michel B <michel.mb.dev@gmail.com>
2022-10-18 11:02:35 +0200

    fix: headers Cross-Origin-Resource-Policy for errors cors

--- app.js
+++ app.js
@@ -16,20 +16,41 @@ import { ConnectDB } from "./config/database.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-/* Create a new instance of express */
-const app = new express();
-app.use(helmet());
-
 /* Config path for Environments Variables */
 dotenv.config({ path: ".env" });
 
+/* Create a new instance of express */
+const app = express();
+
+/* Connection to Database */
+ConnectDB(
+  process.env.USER_DB,
+  process.env.PASS_DB,
+  process.env.SERVER_DB,
+  process.env.NAME_DB
+)
+  .then(() =>
+    console.log("[mongoDb] Connected to database : " + process.env.NAME_DB)
+  )
+  .catch((err) => {
+    console.log(
+      "[mongoDb] Failed to connect !!! --> " +
+        [err.message] +
+        " code:" +
+        err.code
+    );
+    process.exit(1);
+  });
+
 /* Middlewares */
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /* Allows the server to accept requests from any origin. ( CORS ) */
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cross-Origin-Resource-Policy", "same-site");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
@@ -57,28 +78,8 @@ fs.access("images", function (error) {
 });
 
 /* Routes */
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/auth", authRoutes);
 app.use("/api/sauces", saucesRoutes);
-app.use("/images", express.static(path.join(__dirname, "images")));
-
-// Connection to Database
-ConnectDB(
-  process.env.USER_DB,
-  process.env.PASS_DB,
-  process.env.SERVER_DB,
-  process.env.NAME_DB
-)
-  .then(() =>
-    console.log("[mongoDb] Connected to database : " + process.env.NAME_DB)
-  )
-  .catch((err) => {
-    console.log(
-      "[mongoDb] Failed to connect !!! --> " +
-        [err.message] +
-        " code:" +
-        err.code
-    );
-    process.exit(1);
-  });
 
 export default app;
