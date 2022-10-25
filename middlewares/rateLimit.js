99314a842319a2029a5d3d603053164cbf9eed1a
Michel B <michel.mb.dev@gmail.com>
2022-10-25 20:53:53 +0200

    fix: limiter time and max request

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -1,12 +1,12 @@
 import rateLimit from "express-rate-limit";
 
 const limitReached = (req, res) => {
-  res.status(429).json({ message: "Too many requests. Try again later." });
+  res.status(429).json({ message: "Too many requests, please try again in an hour!" });
 };
 
 export const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 3, // Limit each IP to 3 requests per `window` (here, per 15 minutes)
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 100,
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   handler: limitReached,
